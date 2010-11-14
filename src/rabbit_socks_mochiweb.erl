dd9fad2ccaec873d0d61f70a8580057adcda9e43
Michael Bridgen <mikeb@squaremobius.net>
2010-11-14 17:02:26 +0000

    WebSocket handshake (two varieties: 75 and 76)

--- src/rabbit_socks_mochiweb.erl
+++ src/rabbit_socks_mochiweb.erl
@@ -8,21 +8,60 @@ start() ->
     mochiweb_http:start([{name, ?MODULE}, {port, 5975}, {loop, fun loop/1}]).
 
 loop(Req) ->
-    case process_headers(Req) of
+    case process_handshake(Req) of
         {error, _DoesNotCompute} ->
             close_error(Req);
-        {response, Protocol, Headers} ->
+        {response, Protocol, Headers, Body} ->
             send_headers(Req, Headers),
+            Req:send([Body]),
             start_socket(Protocol, Req)
     end.
 
 %% Process the request headers and work out what the response should be
-process_headers(Req) ->
-    {response, rabbit_socks_echo, [{"Upgrade", "WebSocket"}]}.
+process_handshake(Req) ->
+    Origin = Req:get_header_value("origin"),
+    Protocol = Req:get_header_value("websocket-protocol"),
+    Host = Req:get_header_value("Host"),
+    Location = "ws://localhost:5975/",
+    FirstBit = [{"Upgrade", "WebSocket"},
+                {"Connection", "Upgrade"}],
+    case Req:get_header_value("Sec-WebSocket-Key1") of
+        undefined ->
+            {response, rabbit_socks_echo, 
+             FirstBit ++
+                 [{"WebSocket-Origin", Origin},
+                  {"WebSocket-Location", Location}],
+            <<>>};
+        Key1 ->
+            Key2 = Req:get_header_value("Sec-WebSocket-Key2"),
+            Key3 = Req:recv(8),
+            Hash = handshake_hash(Key1, Key2, Key3),
+            {response, rabbit_socks_echo,
+             FirstBit ++
+                 [{"Sec-WebSocket-Origin", Origin},
+                  {"Sec-WebSocket-Location", Location}],
+             Hash}
+    end.
+
+handshake_hash(Key1, Key2, Key3) ->
+    erlang:md5([reduce_key(Key1), reduce_key(Key2), Key3]).
+
+reduce_key(Key) when is_list(Key) ->
+    {NumbersRev, NumSpaces} = lists:foldl(
+                             fun (32, {Nums, NumSpaces}) ->
+                                     {Nums, NumSpaces + 1};
+                                 (Digit, {Nums, NumSpaces})
+                                   when Digit > 47 andalso Digit < 58 ->
+                                     {[Digit | Nums], NumSpaces};
+                                 (_Other, Res) ->
+                                     Res
+                             end, {[], 0}, Key),
+    OriginalNum = list_to_integer(lists:reverse(NumbersRev)) div NumSpaces,
+    <<OriginalNum:32/big-unsigned-integer>>.
 
 %% Write the headers to the response
 send_headers(Req, Headers) ->
-    Req:start_raw_response({101, mochiweb_headers:from_list(Headers)}).
+    Req:start_raw_response({"101 Web Socket Protocol Handshake", mochiweb_headers:from_list(Headers)}).
 
 %% Close the connection with an error.
 close_error(Req) ->
